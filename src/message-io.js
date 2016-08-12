acda07dc7652f471d1fe820cfb6886596b4e02ae
Simon Hooker <simon@mso.net>
2016-08-12 08:55:23 +0100

    Reverting change to try and make the socket more noisy

--- src/message-io.js
+++ src/message-io.js
@@ -100,7 +100,6 @@ module.exports = class MessageIO extends EventEmitter {
         if (verifyError) {
           this.securePair.destroy();
           this.socket.destroy(verifyError);
-          this.socket.emit('error', verifyError);
           return;
         }
       }
